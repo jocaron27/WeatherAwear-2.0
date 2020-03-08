429883bc38c8776556d9af5aabec2050a53a2235
Jeanne Castillo <jcaron27@gmail.com>
2020-03-08 14:42:18 -0700

    configure webpack, grunt client commands, react setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+
+  plugins: isDev ? [new LiveReloadPlugin({appendScriptTag: true})] : []
+}
